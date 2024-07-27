d62432096a327c8db3a2199b98edbb33e667a401
ScacchiPA <scacchipa@gmail.com>
2024-07-27 18:21:42 -0300

    Fix ScreenLogin

--- app/src/main/java/ar/com/scacchipa/eldarwallet/ui/login/LoginScreen.kt
+++ app/src/main/java/ar/com/scacchipa/eldarwallet/ui/login/LoginScreen.kt
@@ -1,12 +1,17 @@
 package ar.com.scacchipa.eldarwallet.ui.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ar.com.scacchipa.eldarwallet.R
 import ar.com.scacchipa.eldarwallet.ui.StandardTextField
@@ -40,27 +45,35 @@ fun LoginScreen(
             labelText = "User name",
             drawableIconId = R.drawable.password_svgrepo_com
         )
+        Spacer(modifier = Modifier.height(20.dp))
         Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             enabled = data.isRegisterButtonEnabled,
             onClick = { viewModel.onRegisterButtonPushed() }) {
             Text(text = "Register")
         }
         Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             enabled = data.isUnregisterButtonEnabled,
             onClick = { viewModel.onUnregisterButtonPushed() }) {
             Text("Unregister")
         }
+        Spacer(modifier = Modifier.height(20.dp))
         Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             enabled = data.isLoginButtonEnable,
             onClick = { viewModel.onLogInButtonPushed() }) {
             Text(text = "Log in")
         }
         Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             enabled = data.isLogOutButtonEnable,
             onClick = { viewModel.onLogOutButtonPushed() }) {
             Text(text = "Log out")
         }
+        Spacer(modifier = Modifier.height(40.dp))
         Switch(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             checked = data.isLogged,
             onCheckedChange = {
                 viewModel.onSwitchTapped(it)
